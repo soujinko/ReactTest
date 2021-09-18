73a8ef29c27d048800a0f2423060546c58d6ee88
soujinko <soujinko@naver.com>
2021-09-19 00:12:28 +0900

    useEffect

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 const SimpleHabit = (props) => {
     const [count, setCount] = useState(0);
+    const spanRef = useRef();
 
-    const handleIncrement = () => {
+    const handleIncrement = useCallback(() => {
         setCount(count + 1);
-    }
+    })
+    useEffect(()=> {
+        console.log(`mounted & updated!: ${count}`)
+    }, [count])
+    // 뒤에 배열 인자 넣어서 해당 값이 변할 때에만 호출이 되게, 빈배열시에 처음에만 호출
     return (
         <li className='habit'>
-        <span className="habit-name">Reading</span>
+        <span ref={spanRef} className="habit-name">Reading</span>
         <span className="habit-count">{count}</span>
         <button className="habit-button habit-increase" onClick={handleIncrement}
         >
